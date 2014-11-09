f7b170092640d6ea9c7d2c5b4b9e857a624400de
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2014-11-10 01:10:18 +0900

    0.2.1

--- src/javascript/release_notes.js
+++ src/javascript/release_notes.js
@@ -1,9 +1,17 @@
 window.RELEASE_NOTES = [
   {
+    version: '0.2.1',
+    date: 'November 10, 2014',
+    changes: [
+      'Apply Markdown syntax highlighting by highlight.js',
+      'Add OGP tags'
+    ]
+  },
+  {
     version: '0.2.0',
     date: 'November 9, 2014',
     changes: [
-      'Apply markdown renderer to messages'
+      'Apply Markdown rendering to messages by marked and github-markdown-css'
     ]
   },
   {
