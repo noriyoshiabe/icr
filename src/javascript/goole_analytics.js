bbadadb21697279694b34bc81738426524f4c269
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2014-12-07 03:42:36 +0900

    google analytics

---
+++ src/javascript/goole_analytics.js
@@ -0,0 +1,13 @@
+(function() {
+  if (location.origin != 'https://instant-chat-room.herokuapp.com') {
+    return
+  }
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57414502-1', 'auto');
+  ga('send', 'pageview');
+})();
